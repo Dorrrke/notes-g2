549dd608b8bd2a2edd38bebdb19d008806eeddb0
Mikhail Gerasimov <m.gerasimov@spacevm.org>
2025-04-24 20:29:03 +0300

    add start server

--- cmd/notes/main.go
+++ cmd/notes/main.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Dorrrke/notes-g2/internal/server"
+)
 
 func main() {
+
+	server := server.New("0.0.0.0", "8080")
+	server.Run()
 	//TODO: Сделать конфигурацию
 	panic(fmt.Errorf("not implemented"))
 }
