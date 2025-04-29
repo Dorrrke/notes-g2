61e47d4892b7377b65dfaedfd0dc76f28f37c1c5
Mikhail Gerasimov <m.gerasimov@spacevm.org>
2025-04-29 21:14:12 +0300

    add in memory repo + buisness logic

--- internal/infrastructure/in-memory/in_memory.go
+++ internal/infrastructure/in-memory/in_memory.go
@@ -1,4 +1,38 @@
 package inmemory
 
+import (
+	usersDomain "github.com/Dorrrke/notes-g2/internal/domain/users"
+)
+
+var emptyUser = usersDomain.User{}
+
 type InMemory struct {
+	userStorage map[string]usersDomain.User
+}
+
+func New() *InMemory {
+	return &InMemory{
+		userStorage: make(map[string]usersDomain.User),
+	}
+}
+
+func (im *InMemory) SaveUser(user usersDomain.User) error {
+	for _, us := range im.userStorage {
+		if us.Email == user.Email {
+			return usersDomain.ErrUserAlredyExists
+		}
+	}
+
+	im.userStorage[user.UID] = user
+	return nil
+}
+
+func (im *InMemory) GetUser(login string) (usersDomain.User, error) {
+	for _, us := range im.userStorage {
+		if us.Email == login {
+			return us, nil
+		}
+	}
+
+	return emptyUser, usersDomain.ErrUserNotFound
 }
