cf3ff218bc142dd2486ab6b6216cb262f252948b
Mikhail Gerasimov <m.gerasimov@spacevm.org>
2025-05-06 22:00:59 +0300

    add save user in db func

--- cmd/notes/main.go
+++ cmd/notes/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
+
 	"github.com/Dorrrke/notes-g2/internal"
+	dbstorage "github.com/Dorrrke/notes-g2/internal/infrastructure/db-storage"
 	inmemory "github.com/Dorrrke/notes-g2/internal/infrastructure/in-memory"
 	"github.com/Dorrrke/notes-g2/internal/server"
 	"github.com/Dorrrke/notes-g2/pkg/logger"
@@ -14,9 +17,15 @@ func main() {
 
 	log.Info().Msg("service starting")
 
-	inMemoryRepo := inmemory.New()
+	var repo server.Repository
+	var err error
+	repo, err = dbstorage.New(context.Background(), "postgres://user:password@localhost:5432/notes?sslmode=disable")
+	if err != nil {
+		log.Warn().Err(err).Msg("failed to connected to db. Use in memory storage")
+		repo = inmemory.New()
+	}
 
-	notesAPI := server.New(cfg, inMemoryRepo)
+	notesAPI := server.New(cfg, repo)
 
 	if err := notesAPI.Run(); err != nil {
 		log.Error().Err(err).Msg("failed running server")
--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.24.0
 require github.com/gin-gonic/gin v1.10.0
 
 require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/rs/zerolog v1.34.0 // indirect
 )
@@ -31,10 +34,10 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.8.0 // indirect
-	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sys v0.20.0 // indirect
-	golang.org/x/text v0.15.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
