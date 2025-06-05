328a53c820495228b29d10c4fb1277a73401a700
Mikhail Gerasimov <m.gerasimov@spacevm.org>
2025-06-05 20:09:11 +0300

    add error groups

--- cmd/notes/main.go
+++ cmd/notes/main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"context"
+	"errors"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/Dorrrke/notes-g2/internal"
 	dbstorage "github.com/Dorrrke/notes-g2/internal/infrastructure/db-storage"
@@ -10,7 +15,21 @@ import (
 	"github.com/Dorrrke/notes-g2/pkg/logger"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"golang.org/x/sync/errgroup"
 )
+
+func gracefulShutdown(cancel context.CancelFunc) {
+	log := logger.Get()
+
+	c := make(chan os.Signal, 1)
+
+	signal.Notify(c, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGHUP)
+
+	sig := <-c
+
+	log.Info().Msgf("graceful shutdown with signal: %s", sig)
+	cancel()
+}
 
 func main() {
 	cfg, err := internal.ReadConfig()
@@ -19,6 +38,11 @@ func main() {
 	}
 
 	log := logger.Get(cfg.Debug)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go gracefulShutdown(cancel)
 
 	log.Info().Msg("service starting")
 
@@ -38,7 +62,27 @@ func main() {
 
 	notesAPI := server.New(cfg, repo)
 
-	if err := notesAPI.Run(); err != nil {
-		log.Error().Err(err).Msg("failed running server")
+	group, gCtx := errgroup.WithContext(ctx)
+
+	group.Go(func() error {
+		return notesAPI.Run()
+	})
+
+	group.Go(func() error {
+		<-gCtx.Done()
+		if err := notesAPI.Stop(gCtx); err != nil {
+			return err
+		}
+		if err := repo.Close(); err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err := group.Wait(); err != nil {
+		if !errors.Is(err, http.ErrServerClosed) {
+			log.Fatal().Err(err).Msg("service stopped with error")
+		}
 	}
+	log.Info().Msg("service stopped gracefully")
 }
